430276da2768fcc371c1ae3248d3029042b6d791
unknown <muneeb.thekhan222@gmail.com>
2023-03-27 14:13:00 +0500

    upadted deployed uri

--- src/store/services/register.js
+++ src/store/services/register.js
@@ -3,7 +3,8 @@ import { setCookies } from "../../utils";
 import { setRegisterUserEmail } from "../reducers/register";
 import { setUserData } from "../reducers/user";
 
-const basePath = "http://localhost:3002";//here you may use backend url
+// const basePath = "http://localhost:3002";//here you may use backend url
+const basePath = 'http://164.90.160.58:3002/'
 
 export const register = (info) => async (dispatch) => {
   try {
