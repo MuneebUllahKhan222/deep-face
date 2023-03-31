f3085d97053f67e5272bde1d098bb7bb6336855c
unknown <muneeb.thekhan222@gmail.com>
2023-04-01 00:04:18 +0500

    updated url

--- src/store/services/user.js
+++ src/store/services/user.js
@@ -3,9 +3,9 @@ import { getCookies } from "../../utils";
 
 // const baseAiPath = "http://192.168.100.14:5000"; // transworld
 // const baseAiPath = "http://192.168.18.30:5000"; // storm
-const baseAiPath = "http://164.90.160.58:5000";
+const baseAiPath = "http://deepduck.ai/backend";
 // const basePath = "http://localhost:3002"
-const basePath = 'http://164.90.160.58:3002'
+const basePath = 'http://deepduck.ai/web-backend'
 
 
 export const imageUploader = (source, target, token) => async (dispatch) => {
